66a1b31b5a53de6150c99568e6261e8a6e206f62
mirsad.bralj <mirsad.bralj@cor-net.net>
2022-10-21 11:36:05 +0200

    design changes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,7 +10,7 @@ import { MatchesFilterService } from 'src/Services/matches-filter.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
   Sports = new Array<Sport>();
